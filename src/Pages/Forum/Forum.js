bfc117948022fa64971e75d31751eb6018552ec7
Inupama21 <kavisekarais.21@uom.lk>
2024-01-22 19:01:45 +0530

    search bar for the forum

--- src/Pages/Forum/Forum.js
+++ src/Pages/Forum/Forum.js
@@ -1,16 +1,25 @@
+import React from 'react';
 import BasicPage from '../../Components/BasicPage/BasicPage';
+import Sidebar from '../../Components/Sidebar/sidebar'; 
+import './forum.css';
+import { FaSearch } from "react-icons/fa";
 
 export default function Forum() {
-	const Tabs = [
-		{ label:"Home", path:"/"},
-		{ label:"Watchlist", path:"/watchlist"},
-		{ label:"Alert", path:"/alert"},
-	];
+  const Tabs = [
+    { label: "Latest", path: "/" },
+    { label: "Top", path: "/watchlist" },
+    { label: "My Problems", path: "/alert" },
+  ];
 
-	return (
-		<BasicPage tabs={Tabs}>
-			<h1>Forum</h1>
-			<p>This is Forum page content</p>
-		</BasicPage>
-	)
+  return (
+    <BasicPage tabs={Tabs}>
+      <div className='search_bar'>
+        <FaSearch></FaSearch><input className='bar' type='text' placeholder='Search'></input>
+      </div>
+      <Sidebar header="Top Categories">
+        
+      </Sidebar> 
+      
+    </BasicPage>
+  );
 }
