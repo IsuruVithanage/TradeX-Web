f1d62e2cc0e35b2792fb7672cf10911eb7603d5b
AshanSalinda <ashansalinda5@gmail.com>
2024-01-27 01:53:12 +0530

    Portfolio with table

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -3,12 +3,12 @@ import './Table.css'
 
 export default function Table(props) {
   return (
-    <div>
+    <div className='table-container'>
         <table className='main-table'>
             <thead className='table-head'>
                 {props.children[0]}
             </thead>
-            <tbody className='table-body'>
+            <tbody className={`table-body ${(props.hover) ? 'hoverble' : ''}`}>
                 {props.children.slice(1)}
             </tbody>
         </table>
@@ -18,10 +18,18 @@ export default function Table(props) {
 
 export function TableRaw({data}) {
   return (
-    <tr>
-        {(data) && data.map((cell, index) => (
-            <td key={index} className='table-cell'>{cell}</td>
-        ))}
+    <tr className='table-row'>
+        {(data) && data.map((cell, index) => {
+            if (typeof cell === 'object' && cell[0] !== ( undefined || '' )) {
+              cell = 
+                <div> 
+                  <img className='coin-icon' src={cell[0]} alt={cell[1]} /> 
+                  {cell[1]} 
+                </div>;
+            }
+            
+            return <td key={index} className={`table-cell ${(index == 0) ? 'first-column' : ''}`}>{cell}</td>
+        })}
     </tr> 
   )
 }
