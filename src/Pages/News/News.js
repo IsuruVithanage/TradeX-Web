f92baa561ce37330515c965fc73705f1aa1784ff
Your Name <you@example.com>
2024-01-26 11:59:26 +0530

    news bar responsive

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { components } from 'react-select'
 import BasicPage from '../../Components/BasicPage/BasicPage'
 import NewsBar from '../../Components/NewsBar/NewsBar'
+import "./News.css"
 
 
 export default function News() {
   return (
     <BasicPage>         
+        <div className='news-container'>
         <NewsBar/>
         <NewsBar/>
         <NewsBar/>
         <NewsBar/>
         <NewsBar/>
         <NewsBar/>
+        </div>
 
     </BasicPage>
   )
