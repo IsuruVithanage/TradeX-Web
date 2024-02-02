b3187bf96dc56a7c945a4a61c2f2f1e483bf0197
Your Name <you@example.com>
2024-02-02 10:15:05 +0530

    add onclick function for heart icon

--- src/Components/NewsBar/NewsItem.js
+++ src/Components/NewsBar/NewsItem.js
@@ -1,39 +1,45 @@
-import image from '../../Assets/Images/image.jpg'
-import './NewsItem.css'
-import { FaRegHeart } from "react-icons/fa6";
-import { AiOutlineDislike } from "react-icons/ai";
-import { AiOutlineLike } from "react-icons/ai";
-
-
+import React, { useState } from 'react';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { AiOutlineDislike, AiOutlineLike } from 'react-icons/ai';
+import image from '../../Assets/Images/image.jpg';
+import './NewsItem.css';
 
 const NewsItem = ({ title, description, src, url }) => {
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFilled((prevIsHeartFilled) => !prevIsHeartFilled);
+  };
+
   return (
     <div className='newsbar-container'>
-
-      <div className='img-container'>  <img src={src ? src : image} alt="..." /></div>
+      <div className='img-container'>
+        <img src={src ? src : image} alt="..." />
+      </div>
       <div className='desc-container'>
-        <div style={{display:"flex"}}>
-            <div className='news-header-container'>
-              <a href={url}>
-                <h1>{title}</h1>
-              </a>
-              <p>{description ? description.slice(0, 90) : "In a rare discovery, scientists reveal the oldest piece of fossilized skin - The Washington Post,"}</p>
-            </div>
-            <div className='favorite-icon-container'><FaRegHeart /></div>
+        <div style={{ display: 'flex' }}>
+          <div className='news-header-container'>
+            <a href={url}>
+              <h1>{title}</h1>
+            </a>
+            <p>{description ? description.slice(0, 90) : "In a rare discovery, scientists reveal the oldest piece of fossilized skin - The Washington Post,"}</p>
+          </div>
+          <div className='favorite-icon-container' onClick={handleHeartClick}>
+            {isHeartFilled ? <FaHeart /> : <FaRegHeart />}
+          </div>
         </div>
 
-        <div className='like-icon-container'><AiOutlineDislike /><AiOutlineLike />
-
-</div>
+        <div className='like-icon-container'>
+          <AiOutlineDislike />
+          <AiOutlineLike />
+        </div>
 
         <div className='foter-bar'>
           <p>1 day</p>
         </div>
       </div>
-
     </div>
+  );
+};
 
-
-  )
-}
-export default NewsItem
+export default NewsItem;
