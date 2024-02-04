df766972616549a0a0afd9049c837321f91cb25a
Inupama21 <kavisekarais.21@uom.lk>
2024-02-05 01:29:18 +0530

    Forum-mainpage

--- src/Pages/Forum/Forum.js
+++ src/Pages/Forum/Forum.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import BasicPage from '../../Components/BasicPage/BasicPage';
 import { RiSoundModuleLine } from "react-icons/ri";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+  
+} from "react-router-dom";
 
 
 
 import './forum.css';
 import { FaSearch } from "react-icons/fa";
 import Detailed from '../../Components/Questionbar/Detailed';
+import { color } from '@mui/system';
+import Questionset from './Questionset';
+
 
 
 
@@ -21,6 +30,7 @@ export default function Forum() {
   
   return (
     <BasicPage tabs={Tabs}>
+  <div className='container'>
     <div className='component1'>
       <div className='search_bar'>
         <FaSearch/>
@@ -30,19 +40,30 @@ export default function Forum() {
         <RiSoundModuleLine className="filter-icon"></RiSoundModuleLine>
       </div>
 
-     {/* the table 
+     {/* the table */}
 
-    <div className='questionrow'>
-        <div  className="questionarea">
-          <h3>Understanding Cryptocurrency Wallet Security</h3>
-        </div>  
-        <div className='questionstat'><span></span><p>0</p></div>
-        <div className='questionstat'><span></span><p>1</p></div>
-        <div className='questionstat'><span></span><p>2</p></div>
-        
+     <div className='topic-row'>
+        <div className='topic'>
+            <h4>Topic</h4>
+        </div>
+        <div className='topic-stat'>
+            <h4>Views</h4>
+        </div>
+        <div className='topic-stat'>
+            <h4>Likes</h4>
+        </div>
+        <div className='topic-stat'>
+            <h4>Replies</h4>
+        </div>
     </div>
-  */}
-    <Detailed/>
+
+
+    <Questionset/>
+    <Questionset/>
+    <Questionset/>
+    
+  
+    {/*<Detailed/>*/}
    
     </div> 
 
@@ -55,8 +76,8 @@ export default function Forum() {
 
     </div>
       
-  
-
-    </BasicPage>
+    
+    </div>
+  </BasicPage>
   );
 }
