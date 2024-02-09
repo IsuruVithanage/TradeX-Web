d4ddfaea33667ce565e8c400fca0c91d31db8630
Your Name <you@example.com>
2024-02-09 15:29:02 +0530

    add new button for welcome  page

--- src/Pages/ExternalWallet/Welcome/Welcome.js
+++ src/Pages/ExternalWallet/Welcome/Welcome.js
@@ -10,9 +10,15 @@ export default function Welcome() {
 
       <div className='head-line'>TradeX &nbsp;<div className='head-line2'> Wallet</div></div>
 
+      <div className='platform'>SIMULATION PLATFORM</div>
+
       <div className='para'>      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/>  Lorem Ipsum has been the industry's standard  dummy text ever since the 1500s,<br/> when an unknown printer took a galley of type and
-         scrambled it to make a type specimen book. <br/> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br/> It was popularised in the 1960s </p>
+         scrambled it to make a type specimen book. <br/> It has survived not only five centuries, but also the leap into electronic typesetting,<br/> remaining essentially unchanged. <br/> It was popularised in the 1960s </p> 
       </div>
+      <div >
+      <button className='custom-button'>GET START</button>
+      </div>
+
         <img src={WalletImage} alt="Wallet Description" className='wallet-img' />
       
     </div>
