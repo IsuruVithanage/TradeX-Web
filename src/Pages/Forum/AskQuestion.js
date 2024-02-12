b79603e762e05f1f6f471289e1bd511db91f9a0b
Inupama21 <kavisekarais.21@uom.lk>
2024-02-12 19:41:12 +0530

    forum-detailed

---
+++ src/Pages/Forum/AskQuestion.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from 'antd/es/form/Form'
+import Forum from './Forum'
+
+function AskQuestion() {
+  return (
+    <div>
+      This is the
+    </div>
+  )
+}
+
+export default AskQuestion
+ 
