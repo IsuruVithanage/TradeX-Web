f46d6a9e1ae16b845a9ddad3653b9645b311e773
Your Name <you@example.com>
2024-02-13 22:40:45 +0530

    Design Change password page

--- src/Pages/ExternalWallet/LoginPage-1/ChangePassword/ChangePassword.js
+++ src/Pages/ExternalWallet/LoginPage-1/ChangePassword/ChangePassword.js
@@ -13,7 +13,22 @@ export default function ChangePassword() {
       <img src={WalletImage} alt="Wallet Description" className='wallet-img' />
 
       <BlackBar>
+      <h1 className='set-pass'>Change Password</h1>
+        <p className='para'>This password is used to protect your wallet and provide access <br/> to the browser web wallet.</p>
+        
+        <input type="password" placeholder="Enter your new password" className="password-input" />
+        <input type="password" placeholder="Confirm your password" className="confirm-password-input" />
 
+        <div className="terms-checkbox">
+          <input type="checkbox" id="terms-checkbox" />
+          <label htmlFor="terms-checkbox">I have read and agree to the Terms of Service</label>
+        </div>
+        <div >
+      <button className='next-button' >Next</button>
+      </div>
+      <div >
+      <button className='back-button' >Back</button>
+      </div>
       </BlackBar>
     </div>
   )
