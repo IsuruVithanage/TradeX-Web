14fa5b8f3852a0b0dffbd37ff5496deeb5692d6b
Your Name <you@example.com>
2024-02-17 16:42:49 +0530

    Design dashboard page

--- src/Pages/ExternalWallet/DashBoard/DashBoard.js
+++ src/Pages/ExternalWallet/DashBoard/DashBoard.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import BasicPage from '../../../Components/BasicPage/BasicPage';
+import "./DashBoard.css";
 
 export default function DashBoard() {
   return (
-    <div>DashBoard</div>
+    <div className='main-background'>
+      <BasicPage sideNavBar = {false}>
+        
+      </BasicPage>
+    </div>
   )
 }
