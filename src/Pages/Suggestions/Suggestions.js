cdb3903daff619c8dbb32f12e1db227576102e43
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-02-25 23:59:45 +0530

    Suggetion tab coin data selection using row select

--- src/Pages/Suggestions/Suggestions.js
+++ src/Pages/Suggestions/Suggestions.js
@@ -11,11 +11,23 @@ import Table, {TableRow} from "../../Components/Table/Table";
 import assets from "../Portfolio/assets.json";
 import initialData from "../Portfolio/portfolio-data.json";
 import LineChart from "../../Components/Charts/LineChart/LineChar";
+import axios from "axios";
 
 export default function Suggestions() {
     const Tabs = [
         {label: "Suggestions", path: "/suggestion"},
     ];
+
+    const [coinData, setcoinData] = useState({
+        name: '',
+        price: 0,
+        symbol: '',
+        marketcap: 0,
+        volume: 0,
+        image: '',
+        priceChange: 0,
+    });
+
 
     const priceLimits = ['Limit', 'Market', 'Stop Limit'];
 
@@ -24,6 +36,36 @@ export default function Suggestions() {
     const handleCoinSelection = (coin) => {
         setSelectedCoin(coin);
     };
+
+    const formatCurrency = (amount) => {
+        const amountString = amount.toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 20
+        });
+        return '$ ' + amountString;
+    };
+
+    function handleRowClick(coin) {
+        console.log(coin);
+        axios
+            .get(
+                `https://api.coingecko.com/api/v3/coins/${coin}`
+            )
+            .then(res => {
+                setcoinData((prevData) => ({
+                    ...prevData,
+                    name: res.data.name,
+                    price: formatCurrency(res.data.market_data.current_price.usd),
+                    symbol: res.data.image.large,
+                    priceChange: res.data.market_data.price_change_24h,
+                    marketcap: res.data.market_data.market_cap.usd,
+                }));
+                console.log(res.data);
+            })
+            .catch(error => console.log(error));
+
+
+    }
 
     return (
         <BasicPage tabs={Tabs}>
@@ -39,7 +81,28 @@ export default function Suggestions() {
                 }
             >
 
-                <CoinBar onSelectCoin={handleCoinSelection} enableModel={false}/>
+                <div className='coinDiv'>
+                    <div className='coin-logo'>
+                        <div className='coin-logo coinimg'>
+                            <img src={coinData.symbol} alt=""/>
+                            <p>{coinData.name}</p>
+                        </div>
+                    </div>
+                    <div className='coinData'>
+                        <div className='cdata'>
+                            <h1>Price</h1>
+                            <p>{coinData.price}</p>
+                        </div>
+                        <div className='cdata'>
+                            <h1>24h Price Change</h1>
+                            <p style={{color: coinData.priceChange > 0 ? "#21DB9A" : "#FF0000"}}>{coinData.priceChange} %</p>
+                        </div>
+                        <div className='cdata'>
+                            <h1>Market Cap</h1>
+                            <p>{coinData.marketcap}</p>
+                        </div>
+                    </div>
+                </div>
                 <LineChart data={initialData}></LineChart>
             </SidePanelWithContainer>
 
@@ -65,6 +128,7 @@ export default function Suggestions() {
                             assets[coin].TotalBalance,
                             assets[coin].marketPrice,
                         ]}
+                        onClick={() => handleRowClick(assets[coin].name)}
                     />
                 ))}
             </Table>
