7cdb77407376f58c583b315412f1882d5268719f
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-03-05 12:01:22 +0530

    Time out meodel and choosed answers saved to an array

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Modal } from 'antd';
+import {Modal} from 'antd';
 import './Modal.css'
 
 export default function Model(props) {
 
-  return (
-    <>
-      <Modal 
-          open={props.open}  
-          onCancel={() => props.close(false)} 
-          footer={false}
-          destroyOnClose={true}>
-            { props.children }
-      </Modal>
-    </>
-  );
+    return (
+        <>
+            <Modal
+                open={props.open}
+                onCancel={() => props.close(false)}
+                footer={false}
+                closable={props.closable !== undefined ? props.closable : true}
+                destroyOnClose={true}>
+                {props.children}
+            </Modal>
+        </>
+    );
 };
--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -4,9 +4,14 @@ import QuestionBar from "../../Components/QuizComponents/QuestionBar";
 import './Quiz.css';
 import QuizTImer from "../../Components/QuizComponents/QuizTImer";
 import axios from "axios";
+import QuizTimer from "../../Components/QuizComponents/QuizTImer";
+import Table, {TableRow} from "../../Components/Table/Table";
+import Modal from "../../Components/Modal/Modal";
 
 export default function Quiz() {
     const [questions,setQuestions]=useState([]);
+    const [isSetterModalOpen, setIsSetterModalOpen] = useState(false);
+    const [answers,setAnswers] = useState([])
 
 
     const Tabs = [
@@ -41,17 +46,36 @@ export default function Quiz() {
         loadQuestions();
     }, []);
 
+    const handleQuizTimeout = () => {
+        setIsSetterModalOpen(true);
+    }
+
+    const getAnswers = (value) => {
+        setAnswers((prevAnswers) => {
+            const newAnswers = [...prevAnswers, value];
+            console.log(newAnswers);
+            return newAnswers;
+        });
+    };
+
+
     return (
         <BasicPage tabs={Tabs}>
             <div className='quiz-container'>
                 <div className='quiz-top'>
                     <p>Level Quiz</p>
-                    <QuizTImer/>
+                    <QuizTimer onTimeout={handleQuizTimeout} />
                 </div>
                 {questions.map((question, index) => (
-                    <QuestionBar key={index} questionNumber={index} question={question} />
+                    <QuestionBar key={index} questionNumber={index} question={question} getAnswers={getAnswers} />
                 ))}
             </div>
+
+            <Modal open={isSetterModalOpen} close={() => handleQuizTimeout()} closable={false}>
+                <div style={{ width: '450px' }}>
+
+                </div>
+            </Modal>
         </BasicPage>
     )
 }
