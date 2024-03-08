ddd8c8c3a8b21913509a27b78765f6e18e7a06af
AshanSalinda <ashansalinda5@gmail.com>
2024-03-09 02:11:33 +0530

    Portfolio Updated with realtime market prices

--- src/index.js
+++ src/index.js
@@ -32,16 +32,8 @@ import HaveAccount from './Pages/ExternalWallet/LoginPage-1/HaveAccount/HaveAcco
 import ChangePassword from './Pages/ExternalWallet/LoginPage-1/ChangePassword/ChangePassword'
 import MyProblems from './Pages/Forum/MyProblems';
 import Dailysummary from './Pages/Summary/Dailysummary';
-<<<<<<< HEAD
 import Education  from './Pages/Education/Education';
 import  Favorites from './Pages/Education/Favorites';
-
-
-
-
-
-
-=======
 import Suggestions from "./Pages/Suggestions/Suggestions";
 import Quiz from "./Pages/Quiz/Quiz";
 import UserProfileTab from "./Pages/User/UserProfileTab";
@@ -50,7 +42,6 @@ import Monthlysummary from './Pages/Summary/Monthlysummary';
 import MyAnswers from './Pages/Forum/MyAnswers';
 import Detailed from './Components/Questionbar/Detailed';
 import History from './Pages/ExternalWallet/History/History';
->>>>>>> 0c9b8e98ec48d0c5e3f13d5ab905390661fd2368
 
 
 const router = createBrowserRouter([
@@ -192,13 +183,11 @@ const router = createBrowserRouter([
     path: "/forum/MyProblems",
     element: <MyProblems/>,
   },
- 
   {
 
     path: "/forum/MyAnswers",
     element: <MyAnswers/>,
   },
-
   {
     path: "/Questionbar/Detailed",
     element: <Detailed />,
@@ -211,12 +200,6 @@ const router = createBrowserRouter([
     path: "/wallet/login/changepassword",
     element: <ChangePassword/>,
   },
-<<<<<<< HEAD
-  
- 
-
-
-=======
   {
     path: "/Questionbar/Detailed",
     element: <Detailed />,
@@ -233,7 +216,6 @@ const router = createBrowserRouter([
     path: "/verify",
     element: <VerifyUser/>,
   },
->>>>>>> 0c9b8e98ec48d0c5e3f13d5ab905390661fd2368
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
