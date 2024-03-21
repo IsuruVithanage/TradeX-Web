18c590cb5804e073e2a200be434ab76efaac53b9
AshanSalinda <ashansalinda5@gmail.com>
2024-03-22 01:46:08 +0530

    updated

--- src/Pages/Portfolio/PortfolioWallet/PortfolioWallet.js
+++ src/Pages/Portfolio/PortfolioWallet/PortfolioWallet.js
@@ -23,6 +23,7 @@ export default function FundingWallet() {
     const [ isLoading, setIsLoading ] = useState(true);
     const backendApiEndpoint = 'http://localhost:8011/portfolio/asset/';
     const userId = 1;
+    const myname = "ashan";
 
 
 
