03ca4b1f26768a1b12d91b3bc2c77783a0736e3e
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-03-31 20:59:20 +0530

    Validation Using Yup and React-Hook-Form

--- src/Components/Input/NumberInput/NumberInput.js
+++ src/Components/Input/NumberInput/NumberInput.js
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
-import { InputNumber } from 'antd';
+import React, {useState} from 'react';
+import {InputNumber} from 'antd';
 import './NumberInput.css'
 
 export default function NumberInput(props) {
     const [step, setStep] = useState('1');
-    
-    const handleStep = (value)=> {
-        
+    const {name, register, errors} = props
+
+    const handleStep = (value) => {
+
         if (value && value.includes('.')) {
             const decimalCount = value.split('.')[1].length;
-            setStep ( '0.'.padEnd(decimalCount + 1, '0') + '1' )
+            setStep('0.'.padEnd(decimalCount + 1, '0') + '1')
         } else {
             setStep('1')
-        } 
+        }
     }
 
 
     return (
 
-        <InputNumber 
-            variant='borderless'
-            prefix={props.icon} 
-            type='number'
-            step={step}  
-            min={props.min}
-            max={props.max}
-            style={props.style} 
-            id={props.id}
-            name={props.name}
-            value={props.value}
-            defaultValue={props.value}
-            placeholder={props.placeholder}
-            onBlur={props.onBlur}
-            onChange={props.onChange && props.onChange}
-            onInput={handleStep} 
-        />
+        <div>
+            <InputNumber
+                variant='borderless'
+                prefix={props.icon}
+                type='number'
+                step={step}
+                min={props.min}
+                max={props.max}
+                style={props.style}
+                id={props.id}
+                name={props.name}
+                value={props.value}
+                {...register(name)}
+                defaultValue={props.value}
+                placeholder={props.placeholder}
+                onBlur={props.onBlur}
+                onChange={props.onChange && props.onChange}
+                onInput={handleStep}
+            />
+            <p style={{color: 'red'}}>{errors[name]?.message}</p>
+        </div>
 
     );
 }
