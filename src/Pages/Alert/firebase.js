31db01bb2518bd783625a4bee4be1a8f7a64657f
AshanSalinda <ashansalinda5@gmail.com>
2024-06-02 03:16:40 +0530

    Updated

--- src/Pages/Alert/firebase.js
+++ src/Pages/Alert/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage, isSupported } from "firebase/messaging";
-import firebaseConfig from "./firebaseConfig.json";
 import alertOperations from "./alertOperations";
 
 
@@ -8,15 +7,24 @@ class Firebase {
     constructor(userId) {
         isSupported().then((isSupported) => {
             if (isSupported) {
+                const firebaseConfig = {
+                    apiKey: process.env.REACT_APP_FCM_API_KEY,
+                    authDomain: process.env.REACT_APP_FCM_AUTH_DOMAIN,
+                    projectId: process.env.REACT_APP_FCM_PROJECT_ID,
+                    storageBucket: process.env.REACT_APP_FCM_STORAGE_BUCKET,
+                    messagingSenderId: process.env.REACT_APP_FCM_MESSAGING_SENDER_ID,
+                    appId: process.env.REACT_APP_FCM_APP_ID,
+                };
+
                 const app = initializeApp(firebaseConfig);
                 this.messaging = getMessaging(app);
                 this.getToken();
                 this.userId = userId;
+
                 this.onMessage((payload) => {
                     new Notification(payload.notification.title, {
                         body: payload.notification.body,
                         icon: payload.notification.icon,
-                        badge: 'https://raw.githubusercontent.com/IsuruVithanage/TradeX-Web/dev/src/Assets/Images/TradeX-mini-logo.png'
                     });
                 });
             }
@@ -62,7 +70,7 @@ class Firebase {
             await this.requestPermission(false);
 
             getToken(this.messaging, {
-                vapidKey: firebaseConfig.vapidKey,
+                vapidKey: process.env.REACT_APP_FCM_VAPID_KEY,
             })
             .then((deviceToken) => {
                 this.deviceToken = deviceToken;
