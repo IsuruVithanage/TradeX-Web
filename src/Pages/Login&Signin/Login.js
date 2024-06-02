00cd5d7fc596ec3fbac7db44dee802d86fab3b8d
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-06-02 16:32:02 +0530

    Add authentication with JWT

--- src/Pages/Login&Signin/Login.js
+++ src/Pages/Login&Signin/Login.js
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaArrowRight } from "react-icons/fa";
-import "./Login.css";
+import axios from "axios";
 import trade from "../../Assets/Images/trade.png";
 import BasicPage from "../../Components/BasicPage/BasicPage";
-import "./LoginValidation";
-import Validation from "./LoginValidation";
-import axios from "axios";
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
 
   const [values, setValues] = useState({
-    username: "",
+    email: "",
     password: "",
   });
 
@@ -28,72 +25,69 @@ function Login() {
     event.preventDefault();
 
     console.log(values);
-    axios.post("http://localhost:8004/user/login", values).then((res) => {
-      console.log("Login success");
-      // Navigate to the home page upon successful login
-      navigate("/home");
-    });
+    axios.post("http://localhost:8004/user/login", values)
+        .then((res) => {
+          const token = res.data.token;
+          console.log("Token:", token);
+          localStorage.setItem("access-token", token);
+          console.log("Login success");
+          navigate("/watchlist");
+        })
+        .catch((err) => {
+          console.error("Login error:", err);
+        });
   };
 
-  // if(Object.keys(Validation(values)).length === 0){
-  //   navigate('/home');
-  // }
-
   return (
-    <BasicPage
-      sideNavBar={false}
-      icon={<img src={trade} width="73px" alt="tradex" />}
-    >
-      <div>
-        <div className="topic">
-          <h1 className="welcome">WELCOME</h1>
-          <h1 className="traders">TRADERS</h1>
-        </div>
-        <div className="black-background">
-          <form action="" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="User Name"
-              className="uname-input"
-              name="username"
-              onChange={handleInput}
-            />
-            {errors.username && (
-              <span className="text-danger-uname"> {errors.username}</span>
-            )}
-            <input
-              type="password"
-              placeholder="Password"
-              className="password-input"
-              name="password"
-              onChange={handleInput}
-            />
-            {errors.password && (
-              <span className="text-danger-password"> {errors.password}</span>
-            )}
+      <BasicPage
+          sideNavBar={false}
+          icon={<img src={trade} width="73px" alt="tradex" />}
+      >
+        <div>
+          <div className="topic">
+            <h1 className="welcome">WELCOME</h1>
+            <h1 className="traders">TRADERS</h1>
+          </div>
+          <div className="black-background">
+            <form action="" onSubmit={handleSubmit}>
+              <input
+                  type="text"
+                  placeholder="User Name"
+                  className="uname-input"
+                  name="email"
+                  onChange={handleInput}
+              />
+              {errors.email && (
+                  <span className="text-danger-uname"> {errors.email}</span>
+              )}
+              <input
+                  type="password"
+                  placeholder="Password"
+                  className="password-input"
+                  name="password"
+                  onChange={handleInput}
+              />
+              {errors.password && (
+                  <span className="text-danger-password"> {errors.password}</span>
+              )}
 
-            <div className="forgot-password">
-              <label htmlFor="forgot-password">Forgot Password ?</label>
+              <div className="forgot-password">
+                <label htmlFor="forgot-password">Forgot Password ?</label>
+              </div>
+              <button type="submit" className="login-button">
+                Login
+              </button>
+            </form>
+
+            <div className="have-account">
+              <div className="having-text">Do not have an account?</div>
+              <Link to="/Signup">
+                <div className="signup-link">SignUp</div>
+              </Link>
             </div>
-            <button type="submit" className="login-button">
-              Login
-            </button>
-            {/* <div>
-          <Link to={'/home'}>
-              <button type='submit' className='login-button'>Login</button>
-          </Link>
-        </div> */}
-          </form>
-
-          <div className="have-account">
-            <div className="having-text">Do not have an account?</div>
-            <Link to="/Signup">
-              <div className="signup-link">SignUp</div>
-            </Link>
           </div>
         </div>
-      </div>
-    </BasicPage>
+      </BasicPage>
   );
 }
 
