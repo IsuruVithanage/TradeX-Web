af5591d9a9e980788bcec5522413d24f89e161b0
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-06-27 20:18:38 +0530

    Update Portfolio.js

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,4 +1,3 @@
-
 import React, {useState, useEffect} from 'react'
 import {showMessage} from '../../Components/Message/Message';
 import { getUser } from '../../Storage/SecureLs';
@@ -12,32 +11,18 @@ import axios from 'axios';
 
 
 export default function Portfolio() {
-  const [assets, setAssets] = useState([]);
-  const [usdBalance, setUsdBalance] = useState(null);
-  const [portfolioValue, setPortfolioValue] = useState(null);
-  const [percentages, setPercentages] = useState([]);
-  const [initialData, setInitialData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const backendApiEndpoint = "http://localhost:8011/portfolio/asset/overview";
-  const user = getUser();
-  const userId = user && user.id;
+    const [assets, setAssets] = useState([]);
+    const [usdBalance, setUsdBalance] = useState(null);
+    const [portfolioValue, setPortfolioValue] = useState(null);
+    const [percentages, setPercentages] = useState([]);
+    const [initialData, setInitialData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const backendApiEndpoint = 'http://localhost:8011/portfolio/asset/overview';
+    const user = getUser();
+    const userId = user && user.id;
 
-  useEffect(() => {
-    setIsLoading(true);
 
-    axios
-      .get(backendApiEndpoint, { params: { userId } })
 
-<<<<<<< HEAD
-      .then((res) => {
-        setAssets(res.data.assets);
-        setPercentages(res.data.percentages);
-        setInitialData(res.data.historyData);
-        setPortfolioValue(res.data.portfolioValue);
-        setUsdBalance(res.data.usdBalance);
-        setIsLoading(false);
-      })
-=======
     useEffect(() => {
         setIsLoading(true);
         
@@ -53,70 +38,69 @@ export default function Portfolio() {
             setUsdBalance(res.data.usdBalance);
             setIsLoading(false);
         })
->>>>>>> cdde2f59dc332014889ca9e24cd066c500d7f438
 
-      .catch((error) => {
-        setIsLoading(false);
-        setPortfolioValue(0);
-        setUsdBalance(0);
-        console.log("error getting assets");
+        .catch(error => {
+            setIsLoading(false);
+            setPortfolioValue(0);
+            setUsdBalance(0);
+            console.log("error getting assets");
 
-        error.response
-          ? showMessage(error.response.status, error.response.data.message)
-          : showMessage("error", "Database connection failed..!");
-      });
-  }, [userId]);
+            error.response ? 
+            showMessage(error.response.status, error.response.data.message)   :
+            showMessage('error', 'Database connection failed..!') ;
+        });
 
-  return (
-    <BasicPage
-      isLoading={isLoading}
-      tabs={[
-        { label: "Overview", path: "/portfolio" },
-        { label: "Trading Wallet", path: "/portfolio/tradingWallet" },
-        { label: "Funding Wallet", path: "/portfolio/fundingWallet" },
-        { label: "History", path: "/portfolio/history" },
-      ]}
-    >
-      <SidePanelWithContainer
-        style={{ height: "75vh" }}
-        header="Composition"
-        sidePanel={<BarChart bars={percentages} />}
-      >
-        <ValueBar usdBalance={usdBalance} portfolioValue={portfolioValue} />
-        <LineChart data={initialData} lineType={2}></LineChart>
-      </SidePanelWithContainer>
+    }, [userId]);
+  
 
-      <Table style={{ marginTop: "1vh" }} emptyMessage="No Assets to show">
-        <TableRow
-          data={[
-            "Coin",
-            "Trading Balance",
-            "Funding Balance",
-            "Total Balance",
-            "market Price",
-            "Value",
-          ]}
-        />
 
-        {assets &&
-          assets.map((asset) => (
-            <TableRow
-              key={asset.symbol}
-              data={[
-                <Coin>{asset.symbol}</Coin>,
-                asset.tradingBalance,
-                asset.fundingBalance,
-                asset.totalBalance,
-                asset.marketPrice,
-                "$ " +
-                  asset.value.toLocaleString("en-US", {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  }),
-              ]}
-            />
-          ))}
-      </Table>
-    </BasicPage>
-  );
+    return (
+        <BasicPage
+            isLoading={isLoading}
+            tabs={[
+                {label: "Overview", path: "/portfolio"},
+                {label: "Trading Wallet", path: "/portfolio/tradingWallet"},
+                {label: "Funding Wallet", path: "/portfolio/fundingWallet"},
+                {label: "History", path: "/portfolio/history"}
+            ]}>
+
+
+            <SidePanelWithContainer
+                style={{height: '75vh'}}
+                header="Composition"
+                sidePanel={<BarChart bars={percentages}/>}>
+                <ValueBar usdBalance={usdBalance} portfolioValue={portfolioValue}/>
+                <LineChart data={initialData} lineType={2}></LineChart>
+            </SidePanelWithContainer>
+
+            <Table style={{marginTop: '1vh'}} emptyMessage="No Assets to show">
+                <TableRow data={[
+                    'Coin',
+                    'Trading Balance',
+                    'Funding Balance',
+                    'Total Balance',
+                    'market Price',
+                    'Value'
+                ]}/>
+
+
+                {assets && assets.map(asset => (
+                    <TableRow
+                        key={asset.symbol}
+                        data={[
+                            <Coin>{asset.symbol}</Coin>,
+                            asset.tradingBalance,
+                            asset.fundingBalance,
+                            asset.totalBalance,
+                            asset.marketPrice,
+                            "$ " + asset.value.toLocaleString("en-US", {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                            })
+                        ]}
+                    />
+                ))}
+            </Table>
+        </BasicPage>
+    )
 }
