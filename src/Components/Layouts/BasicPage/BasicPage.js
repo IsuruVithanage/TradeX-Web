bd8f558a77f317366e015cdc5b2ac8521775b095
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-06-27 20:01:43 +0530

    Update BasicPage.js

--- src/Components/Layouts/BasicPage/BasicPage.js
+++ src/Components/Layouts/BasicPage/BasicPage.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD:src/Components/BasicPage/BasicPage.js
-import React from "react";
-import TopNavBar from "./TopNavBar/TopNavBar";
-import SideNavBar from "./SideNavBar/SideNavBar";
-import { PageLoading } from "../Loading/Loading";
-
-import "./BasicPage.css";
-
-function BasicPage(props) {
-  return (
-    <div>
-      {props.sideNavBar !== false && <SideNavBar />}
-      {props.topNavBar !== false && <TopNavBar {...props} />}
-
-      {props.isLoading && <PageLoading />}
-      {props.isLoading && <div className="page-loading-dimmer" />}
-
-      {props.isLoading && <PageLoading />}
-      {props.isLoading && <div className="page-loading-dimmer" />}
-
-      <div
-        className={`main-container ${
-          props.sideNavBar === false && "full-width"
-        }`}
-      >
-        {props.children}
-      </div>
-    </div>
-  );
-}
-
-export default BasicPage;
-=======
 import React from "react";
 import TopNavBar from "../TopNavBar/TopNavBar";
 import SideNavBar from "../SideNavBar/SideNavBar";
@@ -62,4 +29,3 @@ function BasicPage(props) {
 }
 
 export default BasicPage;
->>>>>>> cdde2f59dc332014889ca9e24cd066c500d7f438:src/Components/Layouts/BasicPage/BasicPage.js
