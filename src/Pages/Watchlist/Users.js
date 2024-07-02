2dc9842e7073f89696f6ee8bfafb18678a581f25
Divya803 <palpolatbd.21@uom.lk>
2024-07-03 03:22:17 +0530

    confirm deletion

--- src/Pages/Watchlist/Users.js
+++ src/Pages/Watchlist/Users.js
@@ -5,10 +5,10 @@ import "./ViewAll.css";
 import axios from "axios";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import Table, { TableRow } from "../../Components/Table/Table";
-
+import Modal from "../../Components/Modal/Modal";
+import Input from "../../Components/Input/Input";
 
 export default function Users() {
-
   const getVerifiedCellStyle = (isVerified) => {
     return isVerified ? { color: "#21DB9A" } : { color: "red" };
   };
@@ -21,6 +21,9 @@ export default function Users() {
     Contact: "",
     Age: "",
   });
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
 
   const loadUsers = async () => {
     try {
@@ -37,19 +40,23 @@ export default function Users() {
     loadUsers();
   }, []);
 
-  const deleteUser = async (userId) => {
+  const confirmDeleteUser = (userId) => {
+    setUserToDelete(userId);
+    setIsDeleteModalOpen(true);
+  };
+
+  const deleteUser = async () => {
     try {
-      await axios.delete(`http://localhost:8004/admin/deleteUser/${userId}`);
-      setUserList(userList.filter(user => user.userId !== userId));
+      await axios.delete(
+        `http://localhost:8004/admin/deleteUser/${userToDelete}`
+      );
+      setUserList(userList.filter((user) => user.userId !== userToDelete));
+      setIsDeleteModalOpen(false);
+      setUserToDelete(null);
     } catch (error) {
       console.error("Error deleting user", error);
     }
   };
-
-
-  useEffect(() => {
-    loadUsers();
-  }, []);
 
   return (
     <BasicPage
@@ -57,33 +64,74 @@ export default function Users() {
         { label: "Dashboard", path: "/admin/AdDashboard" },
         { label: "Users", path: "/admin/Users" },
         { label: "Admin", path: "/admin" },
-
       ]}
     >
       <div>
         <div>
-          <Table
-              hover={true}
-              style={{marginTop:"15px"}}
-            >
-              <TableRow data={["Name", "Email", "Role", "NIC", "Contact", "Delete"]} classes={["col1","col2","col3","col4", "col5","col6"]}/>
-              {userList.map((user) => (
-                <TableRow 
-                  classes={["col1","col2","col3","col4", "col5","col6"]}
-                  key={user.userId}
-                  data={[
-                    user.userName,
-                    <span style={{width: "200px", textAlign:"center"}}>{user.email}</span>,
-                    user.role,
-                    user.nic,
-                    user.phoneNumber,
-                    <RiDeleteBin6Line onClick={() => deleteUser(user.userId)}
-                    style={{ cursor: "pointer", fontSize:"20px" , color:"red"}} />
-                  ]}
-                />
-              ))}
-            </Table>
+          <Table hover={true} style={{ marginTop: "15px" }}>
+            <TableRow
+              data={["Name", "Email", "Role", "NIC", "Contact", "Delete"]}
+              classes={["col1", "col2", "col3", "col4", "col5", "col6"]}
+            />
+            {userList.map((user) => (
+              <TableRow
+                classes={["col1", "col2", "col3", "col4", "col5", "col6"]}
+                key={user.userId}
+                data={[
+                  user.userName,
+                  <span style={{ width: "200px", textAlign: "center" }}>
+                    {user.email}
+                  </span>,
+                  user.role,
+                  user.nic,
+                  user.phoneNumber,
+                  <RiDeleteBin6Line
+                    onClick={() => confirmDeleteUser(user.userId)}
+                    style={{
+                      cursor: "pointer",
+                      fontSize: "20px",
+                      color: "red",
+                    }}
+                  />,
+                ]}
+              />
+            ))}
+          </Table>
         </div>
+
+        {isDeleteModalOpen && (
+          <Modal
+            open={isDeleteModalOpen}
+            close={() => setIsDeleteModalOpen(false)}
+          >
+            <div
+              style={{ width: "300px", margin: "auto", textAlign: "center" }}
+            >
+              <h2>Do you confirm to delete this user?</h2>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  marginTop: "20px",
+                }}
+              >
+                <Input
+                        type="button"
+                        style={{ width: "110px" }}
+                        onClick={deleteUser}
+                        value="Yes"
+                />
+                <Input
+                        type="button"
+                        style={{ width: "110px" }}
+                        onClick={() => setIsDeleteModalOpen(false)}
+                        value="No"
+                        red
+                />
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     </BasicPage>
   );
--- src/Validation/AdminValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const validationSchema = yup.object().shape({
-    AdminName: yup.string().required("Admin Name is required"),
-    email: yup.string().email("Invalid email format").required("Email is required"),
-    NIC: yup.string().required("NIC is required"),
-    Contact: yup.string().required("Contact is required"),
-});
